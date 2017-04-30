33dd9d5a084cce289bbfd96607b4a613508e34ab
Ralphsun73221 <sunzheng-jie@sunzhengjies-MacBook-Air.local>
2017-04-30 22:57:52 +0800

    增加可以顯示改名前跟改名後顯示於終端

--- rename_file.py
+++ rename_file.py
@@ -8,6 +8,10 @@ def rename_file():
     #如果你把這程式跟想更改檔名的當按放在同一個資料夾其實就可以把這段也註解掉
 
     for file_name in file_list:
+        print("Old Name - " + file_name)
+        print("New Name - " + file_name.lstrip("0123456789"))
+        #上面這兩串的用意在於讓你知道他執行的過程，同時會在終端出現舊的名字跟新的名字
+        
         os.rename(file_name, file_name.lstrip("0123456789"))
 
 rename_file()
