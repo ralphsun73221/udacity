9f3087e0c04aa37eb29bbe9da0df023d60430bda
Ralphsun73221 <sunzheng-jie@sunzhengjies-MacBook-Air.local>
2017-04-30 22:44:19 +0800

    加註說明os.chdir

--- rename_file.py
+++ rename_file.py
@@ -5,6 +5,7 @@ def rename_file():
     #print(file_list)
 
     os.chdir("/Users/sunzheng-jie/Desktop/git/udacity/prank")
+    #如果你把這程式跟想更改檔名的當按放在同一個資料夾其實就可以把這段也註解掉
 
     for file_name in file_list:
         os.rename(file_name, file_name.lstrip("0123456789"))
