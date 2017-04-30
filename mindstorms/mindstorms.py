76e4ac40749d37b012615b3f8fbf8b555abb5dba
Ralphsun73221 <sunzheng-jie@sunzhengjies-MacBook-Air.local>
2017-04-30 23:48:16 +0800

    更改烏龜的顏色、速度、形狀

--- mindstorms/mindstorms.py
+++ mindstorms/mindstorms.py
@@ -5,6 +5,11 @@ def draw_square():
     window.bgcolor("red")
     
     brad = turtle.Turtle()
+    brad.shape("arrow")
+    brad.color("blue", "green")
+    brad.speed('normal')
+    #之前寫成turtle.XXX但發現根本不是這樣寫，要寫成brad.XXX才行
+
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
