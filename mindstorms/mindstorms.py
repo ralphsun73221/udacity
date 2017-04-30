fc61b50983a90b04802ae362c7b927dddab91dee
Ralphsun73221 <sunzheng-jie@sunzhengjies-MacBook-Air.local>
2017-05-01 01:03:48 +0800

    加入如何畫三角形

--- mindstorms/mindstorms.py
+++ mindstorms/mindstorms.py
@@ -1,9 +1,13 @@
 import turtle
-
-def draw_square(some_turtle):
-    for i in range(1,5): #這邊編譯器會出現Unused variable'i'的警示，但還是可以執行
+def draw_triangle(some_turtle):
+    for i in range(1,4):
         some_turtle.forward(100)
-        some_turtle.right(90)
+        some_turtle.right(120)
+
+#def draw_square(some_turtle):
+    #for i in range(1,5): #這邊編譯器會出現Unused variable'i'的警示，但還是可以執行
+        #some_turtle.forward(100)
+        #some_turtle.right(90)
         #這串主要用途在於讓原本那一大串重複執行的code可以自動執行四次
 
 def draw_art():
@@ -21,17 +25,19 @@ def draw_art():
         #brad.right(90)
         #這邊是另外一種寫法，直接在同樣的fun裡面直接執行迴圈，好處在於可以不用再寫一個fun，但前提是除非這一串只用這一次。
     
-    for i in range(1,37):
-        draw_square(brad)
-        brad.right(10)
+    #for i in range(1,37):
+        #draw_square(brad)
+        #brad.right(10)
     #重複迴圈讓他畫成一個圓形
     #把上面寫好的def拿過來用，把()填入這邊定義的brad，他就會去用上面的fun套用到bard
+
+    draw_triangle(brad)
     
 
-    angie = turtle.Turtle()
-    angie.shape("turtle")
-    angie.color("yellow")
-    angie.circle(100)
+    #angie = turtle.Turtle()
+    #angie.shape("turtle")
+    #angie.color("yellow")
+    #angie.circle(100)
     #另外一串畫圖，用烏龜畫圈圈
 
     window.exitonclick()
