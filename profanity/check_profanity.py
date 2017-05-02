46db09613959530ae45e60c1bb4094327ce09d7d
Ralphsun73221 <sunzheng-jie@sunzhengjies-MacBook-Air.local>
2017-05-02 23:43:35 +0800

    加入新的程式資料夾 並且把程式修改成可以執行

---
+++ profanity/check_profanity.py
@@ -0,0 +1,22 @@
+import requests
+
+def read_text():
+    quotes = open("/Users/sunzheng-jie/Desktop/git/udacity/profanity/movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    #url = 'http://www.wdylike.appspot.com/?q=' +text_to_check
+    connection = requests.get('http://www.wdylike.appspot.com/?q=' +text_to_check)
+    print(connection.text)
+    connection.close()
+    #按照課程的原本的寫法，應該是import urllib.request
+    #check_profanity的寫法是
+    #connection = urllib.request.urlopen('http://www.wdylike.appspot.com/?q=' +text_to_check)
+    #output = connection.read()
+    #print(output)
+    #可是我怎樣試都只會得到400的錯誤
+
+read_text()
