2170dc3fd0b0048ad418328ffd82b2dd3a5fd25c
Ralphsun73221 <sunzheng-jie@sunzhengjies-MacBook-Air.local>
2017-05-03 13:59:27 +0800

    將原始程式碼進行encoding的動作。

--- profanity/check_profanity.py
+++ profanity/check_profanity.py
@@ -10,17 +10,13 @@ def read_text():
 
 def check_profanity(text_to_check):
     #url = 'http://www.wdylike.appspot.com/?q=' +text_to_check
-    f = urllib.parse.quote(text_to_check)
-    print(f)
-    #connection = requests.get('http://www.wdylike.appspot.com/?q=' +f)
-    #connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+f)
-    #output = connection.read()
-    #print(output)
-    #print(connection.text)
-    #connection.close()
+    connection = requests.get('http://www.wdylike.appspot.com/?q='+ text_to_check)
+    print(connection.text)
+    connection.close()
     #按照課程的原本的寫法，應該是import urllib.request
     #check_profanity的寫法是
-    #connection = urllib.request.urlopen('http://www.wdylike.appspot.com/?q=' +text_to_check)
+    #f = urllib.parse.quote(text_to_check)
+    #connection = urllib.request.urlopen('http://www.wdylike.appspot.com/?q='+f)
     #output = connection.read()
     #print(output)
     #可是我怎樣試都只會得到400的錯誤
