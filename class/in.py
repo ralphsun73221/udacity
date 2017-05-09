d3c73d06d2b0368bfb26074f408726342bdaa7fc
Ralphsun73221 <sunzheng-jie@sunzhengjies-MacBook-Air.local>
2017-05-09 22:35:13 +0800

    都完成了

--- class/in.py
+++ class/in.py
@@ -4,15 +4,22 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - " + self.last_name ) 
+        print("Eye Color - " + self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toy):
         print("Child Constructor Called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toy = number_of_toy
 
-#billy_cyrus = Parent("孫", "棕色")
-#print(billy_cyrus.last_name)
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+        print("Number of toys - " + self.number_of_toy)
 
-miley_cyrus = Child("孫", "棕色", 5)
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_of_toy)
+billy_cyrus = Parent("孫", "棕色")
+#billy_cyrus.show_info()
+miley_cyrus = Child("孫", "棕色", "5")
+miley_cyrus.show_info()
